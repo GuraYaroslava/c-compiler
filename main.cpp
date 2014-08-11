dff987a5b2d2884df58b1f54f97968d1b934cb50
GuraYaroslava <yaroslava_05@mail.ru>
2014-08-12 01:03:08 +1000

    Fix start program.

--- main.cpp
+++ main.cpp
@@ -6,78 +6,85 @@
 #include "_parser.h"
 using namespace std;
 
-void openFile(char* name)
+void openFile(char* name1, char* name2)
 {
-    ifstream inf(name);
-    if (!inf)
+    ifstream fin(name1);
+    ofstream fout(name2);
+    if (!fin)
     {
-        cout << "Error: Can't open file '" << name << "'.\n";
+        cerr << "Error: Can't open file \"" << name1 << "\".\n";
         exit(-1);
     }
-    cout << "File is successfully opened." << endl;
+    if (!fout)
+    {
+        cerr << "Error: Can't open file \"" << name2 << "\".\n";
+        exit(-1);
+    }
+    cout << "Files are successfully opened." << endl;
 }
 
 int main(int argc, char* argv[])
 {
-    ofstream fout("1.in");
-    /*cout << "C Compiler [Version 0.1]" << endl << "Copyright (c) Gura Yaroslava, 2013 " << endl;
+    cout << "C language subset compiler" << endl
+         << "Author: Yaroslava Gura, B8303A, FEFU" << endl;
+
     if (argc <= 1)
     {
         cout << "Usage: " << argv[0] << " <key: -s -sp -p -d -o> <input file name> <output file name>" << endl;
-        exit(1);
+        return EXIT_SUCCESS;
     }
+
+    openFile(argv[2], argv[3]);
     ofstream fout(argv[3]);
-    openFile(argv[2]);*/
+
     try
     {
-        /*if (!strcmp(argv[1], "-s"))
+        //lexical analysis
+        if (!strcmp(argv[1], "-s"))
         {
             Scanner scanner(argv[2]);
-            fout << "+-----------------------------------------------------------------------------------------+" << endl;
-            fout << setw(20) << "Type" << "\t";
-            fout << setw(10) << "Line" << "\t";
-            fout << setw(10) << "Position" << "\t";
-            fout << setw(10) << "Text" << "\t";
-            fout << setw(10) << "Value" << "\t" << endl;
-            fout << "+-----------------------------------------------------------------------------------------+" << endl;
-            while (scanner.HasNext()) 
+            for (BaseToken* token = scanner.Next();
+                 token->GetType() != EOF_;
+                 token = scanner.Next())
             {
-                BaseToken *token = scanner.Next();
-                if (token->GetType() != NEWLINE && token->GetType() != COMMENT)
-                    token->Print(fout);
+                token->Print(fout);
             }
         }
+        //analysis of the simplest expressions
         else if (!strcmp(argv[1], "-sp"))
         {
             SimpleParser parser(argv[2]);
-            Node *root = parser.ParseExpr();
+            Node* root = parser.ParseExpr();
             parser.PrintTree(root, 5, 0, fout);
         }
+        //parsing of expressions
         else if (!strcmp(argv[1], "-p"))
         {
             Parser parser(argv[2]);
-            SyntaxNode *root = parser.ParseExpression(0);
+            SyntaxNode* root = parser.ParseExpression(0);
             parser.PrintTree(root, 10, 0, fout);
-            /*BaseToken *t = new TokenVal <TokenType> ("h", 0,0,OPERATOR, OPERATOR);
-            cout << GetValue <TokenType> (t) ;*/
-        /*}
-        else if (!strcmp(argv[1], "-d_s"))
-        {*/
-            char p[100];
-            cin.getline(p, 100);
-            Parser parser(p);
-            //Parser parser(argv[2]);
-            while (*parser.lexer.Peek() != LX_EOF)
+        }
+        //parsing of declarations
+        else if (!strcmp(argv[1], "-d"))
+        {
+            Parser parser(argv[2]);
+            while (*parser.lexer.Peek() != EOF_)
+            {
                 parser.Parse(parser.stmt);
+            }
             parser.PrintSymbols(cout);
-            int a = 1;
-            +a;
-            -a;
-        //}
+        }
     }
     catch (Exception exp)
     {
-        cout << exp.GetException() << endl;
-        exit(-1);
+        fout << exp.GetException() << endl;
+        return EXIT_FAILURE;
     }
+    catch (...)
+    {
+        cerr << "ERROR: unknown exception.";
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
